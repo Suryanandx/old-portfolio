b1e94f136734b2c9d3853adc1d40beec94766dd7
Suryanandx <suryanandkurup@gmail.com>
2021-05-04 11:20:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import PrimarySearchAppBar from "./components/AppBar/Appbar";
+import Skills from "./components/Skills/Skills";
+
+
+function App() {
+  return (
+    <div className="App">
+      <PrimarySearchAppBar/>
+      <Skills/>
+      
+        
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,15 @@
+import { Grid } from '@material-ui/core'
+import React from 'react'
+
+const Skills = () => {
+    return (
+        <Grid
+        xs={6}
+        lg={4}
+        >
+            <img height='300px' width='500px' src="https://i.ibb.co/njPwWGg/Untitled-design.png" alt=""/>
+        </Grid>
+    )
+}
+
+export default Skills
